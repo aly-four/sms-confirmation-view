196bd9a054ac3bbe810cc937bfb2f9dd1826417e
Eslam <a.aly.salhin@gmail.com>
2022-03-26 20:04:50 +0200

    update

--- library/src/main/java/com/fraggjkee/smsconfirmationview/smsretriever/SmsParser.kt
+++ library/src/main/java/com/fraggjkee/smsconfirmationview/smsretriever/SmsParser.kt
@@ -3,6 +3,7 @@ package com.fraggjkee.smsconfirmationview.smsretriever
 internal object SmsParser {
 
 
+    //hi
     // TODO this might require improvement/refactoring
     fun parseOneTimeCode(message: String, codeLength: Int): String? {
         val regex = "\\d{$codeLength}".toRegex()
